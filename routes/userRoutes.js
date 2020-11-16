5d320a9f5ef9987f1e4510a7c20a34e68f485db9
unknown <nikhilsunil90s@gmail.com>
2020-11-16 16:48:59 +0530

    First Commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,13 +6,18 @@ Routes.get("/" , (req,res,next) => {
     res.render("userView/index")
 });
 
-Routes.get("/displayNumbers" , (req,res,next) => {
+Routes.get("/categories" , (req,res,next) => {
     res.render("userView/categories")
 });
 
 Routes.get("/contactUs" , (req,res,next) => {
     console.log(req.body);
     res.render("userView/contactUs")
+})
+
+Routes.get("/cart" , (req,res,next) => {
+    console.log(req.body);
+    res.render("userView/cart")
 })
 
 
